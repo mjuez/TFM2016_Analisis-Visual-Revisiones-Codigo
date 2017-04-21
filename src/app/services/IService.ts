62a4dd8d3339a83bf4ca34f0124cbc6027b40a58
Mario Juez <mjuezath@gmail.com>
2017-04-21 09:35:44 +0200

    added createOrUpdateMultiple interface. #26

--- src/app/services/IService.ts
+++ src/app/services/IService.ts
@@ -13,4 +13,13 @@ export interface IService<IEntity> {
      *                  entity (or an error if something goes wrong).
      */
     createOrUpdate(entity: IEntity, callback?: (err: any, result: IEntity) => void): void;
+
+    /**
+     * Saves or updates many entities into database.
+     * @param entities  an array of entities.
+     * @param callback  optional callback function to retrieve the created/updated
+     *                  entity array (or an error if something goes wrong).
+     */
+    createOrUpdateMultiple(entities: IEntity[], callback?: (err: any, result: IEntity[]) => void): void;
+    
 }
