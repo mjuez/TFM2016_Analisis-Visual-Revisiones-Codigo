4da01f3fac90eee4488081295325c52c1ce45783
Mario Juez <mjuezath@gmail.com>
2017-05-04 09:09:13 +0200

    added toEntity and toEntityArray #40.

--- src/app/entities/TaskManagerEntity.ts
+++ src/app/entities/TaskManagerEntity.ts
@@ -33,5 +33,31 @@ export class TaskManagerEntity extends AbstractEntity<TaskManagerDocument> imple
     set status(status: TaskManagerStatus) {
         this.document.status = status;
     }
+
+    /**
+     * Transforms raw data to ITaskManagerEntity.
+     * @param data  raw data.
+     * @returns a task manager entity.
+     */
+    public static toEntity(data: any): ITaskManagerEntity {
+        let entity: ITaskManagerEntity = new TaskManagerEntity(<TaskManagerDocument>data);
+        return entity;
+    }
+
+    /**
+     * Transforms raw data to ITaskManagerEntity array.
+     * @param data  raw data.
+     * @returns an array of task manager entities.
+     */
+    public static toEntityArray(data: any[]): ITaskManagerEntity[] {
+        let entityArray: ITaskManagerEntity[] = [];
+        if (data.length > 0) {
+            data.map((jsonObject) => {
+                let entity: ITaskManagerEntity = this.toEntity(jsonObject);
+                entityArray.push(entity);
+            });
+        }
+        return entityArray;
+    }
     
 }
