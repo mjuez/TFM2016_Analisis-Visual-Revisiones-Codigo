0f0cb93880fb663becf34697a2f552ff5cc5c303
Mario Juez <mjuezath@gmail.com>
2017-05-21 23:01:49 +0200

    added top menu #50.

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,8 +1,20 @@
-$.fn.api.settings.api = {
-    'request repository': '/api/remote/{owner}/{repository}/pulls/'
-}
-
 $(document).ready(function () {
+
+    $.fn.api.settings.api = {
+        'request repository': '/api/remote/{owner}/{repository}/pulls/'
+    }
+
+    const activeSection = $(location).prop('pathname').split('/')[1];
+
+    const menuItems = {
+        home: $('#m_home'),
+        repositories: $('#m_repositories'),
+        pullrequests: $('#m_pullrequests'),
+        users: $('#m_users')
+    }
+
+    setActiveMenuItem(menuItems, activeSection);
+    setMenuClickEvents(menuItems)
 
     let requestRepositoryBtn = $('.request-repository');
     requestRepositoryBtn.api({
@@ -17,3 +29,43 @@ $(document).ready(function () {
     });
 
 });
+
+function setActiveMenuItem(menuItems, activeSection) {
+
+    menuItems['home'].removeClass('active');
+    menuItems['repositories'].removeClass('active');
+    menuItems['pullrequests'].removeClass('active');
+    menuItems['users'].removeClass('active');
+
+    let active = 'home';
+    if (activeSection.length > 0) {
+        active = activeSection;
+    }
+
+    menuItems[active].addClass('active');
+
+}
+
+function setMenuClickEvents(menuItems) {
+
+    menuItems.home.on('click', () => {
+        history.pushState(null, 'Inicio', '/');
+        setActiveMenuItem(menuItems, 'home');
+    });
+
+    menuItems.repositories.on('click', () => {
+        history.pushState(null, 'Repositorios', 'repositories');
+        setActiveMenuItem(menuItems, 'repositories');
+    });
+
+    menuItems.pullrequests.on('click', () => {
+        history.pushState(null, 'Pull Requests', 'pullrequests');
+        setActiveMenuItem(menuItems, 'pullrequests');
+    });
+
+    menuItems.users.on('click', () => {
+        history.pushState(null, 'Usuarios', 'users');
+        setActiveMenuItem(menuItems, 'users');
+    });
+    
+}
