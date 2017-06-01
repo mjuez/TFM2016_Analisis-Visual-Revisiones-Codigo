eee92630432e2dcbcd719981ec26b437f4affcf1
Mario Juez <mjuezath@gmail.com>
2017-06-01 15:48:03 +0200

    removed unnecesary code #57.

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,31 +1,10 @@
-const SECTION_HOME = 'home';
-const SECTION_REPOSITORIES = 'repositories';
-const SECTION_PULLREQUESTS = 'pullrequests';
-const SECTION_USERS = 'users';
-const STATUS_RUNNING = 'status_running';
-const STATUS_WAITING = 'status_waiting';
-const STATUS_ERROR = 'status_error';
+var STATUS_RUNNING = 'status_running';
+var STATUS_WAITING = 'status_waiting';
+var STATUS_ERROR = 'status_error';
 
 $(document).ready(function () {
 
-    const _section = $(location).prop('pathname').split('/')[1];
-    let activeSection = 'home';
-    if (_section.length > 0) {
-        activeSection = _section;
-    }
-
-
-    const menuItems = {
-        home: $('#m_home'),
-        repositories: $('#m_repositories'),
-        pullrequests: $('#m_pullrequests'),
-        users: $('#m_users')
-    }
-
     setFooterHeight();
-    //setActiveMenuItem(menuItems, activeSection);
-    //setMenuClickEvents(menuItems);
-    //loadSection(activeSection);
     setStatusInterval();
 
     app.run('#/');
@@ -47,76 +26,6 @@ function setActiveMenuItem(activeSection) {
         $(`#m_${activeSection}`).addClass('active');
     }
 
-}
-
-function setMenuClickEvents(menuItems) {
-
-    menuItems.home.on('click', () => {
-        history.pushState(null, 'Inicio', '/');
-        setActiveMenuItem(menuItems, SECTION_HOME);
-        loadSection(SECTION_HOME);
-    });
-
-    menuItems.repositories.on('click', () => {
-        history.pushState(null, 'Repositorios', '/repositories');
-        setActiveMenuItem(menuItems, SECTION_REPOSITORIES);
-        loadSection(SECTION_REPOSITORIES);
-    });
-
-    menuItems.pullrequests.on('click', () => {
-        history.pushState(null, 'Pull Requests', '/pullrequests');
-        setActiveMenuItem(menuItems, SECTION_PULLREQUESTS);
-        loadSection(SECTION_PULLREQUESTS);
-    });
-
-    menuItems.users.on('click', () => {
-        history.pushState(null, 'Usuarios', '/users');
-        setActiveMenuItem(menuItems, SECTION_USERS);
-        loadSection(SECTION_USERS);
-    });
-
-}
-
-function loadSection(section) {
-    showLoader();
-    switch (section) {
-        case SECTION_HOME:
-            loadHome(hideLoader);
-            break;
-        case SECTION_REPOSITORIES:
-            loadRepositories(hideLoader);
-            break;
-        case SECTION_PULLREQUESTS:
-            loadPullrequests(hideLoader);
-            break;
-        case SECTION_USERS:
-            loadUsers(hideLoader);
-            break;
-        default:
-            showError(hideLoader);
-    }
-}
-
-function loadPage(template, pageList, page){
-    $('#content').load(template, function () {
-        const url = $(location).prop('pathname').split('/');
-        if (url.length > 2) {
-            const isPage = url[2] === 'page';
-            const isSingle = url[2] === 'single';
-            if (isPage) {
-                const page = Number.parseInt(url[3]);
-                pageList(page);
-            } else if (isSingle) {
-                const owner = url[3];
-                const repository = url[4];
-                page(owner, repository);
-            } else {
-                showError(hideLoader);
-            }
-        } else {
-            pageList(1);
-        }
-    });
 }
 
 function showLoader() {
@@ -162,12 +71,12 @@ function printPaginator(paginator, currentPage, numPages, url) {
     paginator.html('');
 
     if (numPages < 12) {
-        for (let page = 1; page <= numPages; page++) {
+        for (var page = 1; page <= numPages; page++) {
             appendPageLink(page);
         }
     } else {
         if (currentPage < 7) {
-            for (let page = 1; page < 10; page++) {
+            for (var page = 1; page < 10; page++) {
                 appendPageLink(page);
             }
             appendDisabledLink();
@@ -175,14 +84,14 @@ function printPaginator(paginator, currentPage, numPages, url) {
         } else {
             appendPageLink(1);
             appendDisabledLink();
-            let numLastPages = numPages - currentPage;
+            var numLastPages = numPages - currentPage;
             if (numLastPages <= 5) {
-                let numPrevVisible = 8 - numLastPages;
-                for (let page = currentPage - numPrevVisible; page <= numPages; page++) {
+                var numPrevVisible = 8 - numLastPages;
+                for (var page = currentPage - numPrevVisible; page <= numPages; page++) {
                     appendPageLink(page);
                 }
             } else {
-                for (let page = currentPage - 3; page <= currentPage + 3; page++) {
+                for (var page = currentPage - 3; page <= currentPage + 3; page++) {
                     appendPageLink(page);
                 }
                 appendDisabledLink();
@@ -193,13 +102,13 @@ function printPaginator(paginator, currentPage, numPages, url) {
     }
 
     function appendPageLink(page) {
-        const isActive = page === currentPage;
-        const a = pageLink(page, isActive);
+        var isActive = (page === currentPage);
+        var a = pageLink(page, isActive);
         paginator.append(a);
     }
 
     function appendDisabledLink() {
-        const disabled = $('<div>', {
+        var disabled = $('<div>', {
             class: 'disabled item',
             html: '...'
         });
@@ -207,9 +116,9 @@ function printPaginator(paginator, currentPage, numPages, url) {
     }
 
     function pageLink(page, isActive) {
-        let cssClass = "item";
+        var cssClass = "item";
         if (isActive) cssClass += " active";
-        const a = $('<a>', {
+        var a = $('<a>', {
             class: cssClass,
             text: page,
             herf: `${url}/${page}`
