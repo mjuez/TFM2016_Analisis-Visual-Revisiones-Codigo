6658b3d6382a958ce1a5da47b61973804d8c0619
Mario Juez <mjuezath@gmail.com>
2017-06-05 13:56:16 +0200

    implemented getUser #62.

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -36,6 +36,36 @@ export class UserService extends AbstractPersistenceService<IUserRepository, IUs
         super(repository);
     }
 
+    public async getUser(username: string): Promise<IUserEntity> {
+        const repo: IUserRepository = this._repository;
+        const filter: Object = { login: username };
+        return repo.findOne(filter);
+    }
+
+    public async getUsersPage(page: number, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async getUsersByNamePage(page: number, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async getUsersByPullRequestsPage(page: number, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async getUsersByReviewsPage(page: number, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async getUsersByReviewsByStatePage(page: number, state: string, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async getUsersByReviewCommentsPage(page: number, direction: number): Promise<IUserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+
     protected async findEntity(entity: IUserEntity): Promise<IUserEntity> {
         return await this._repository.findById(entity.id);
     }
