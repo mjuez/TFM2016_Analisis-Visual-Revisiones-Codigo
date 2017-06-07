5ed402088bcb0b8f82e1ced3ee66898df4d55f4a
Mario Juez <mjuezath@gmail.com>
2017-06-07 12:41:16 +0200

    reviews stats api route #65.

---
+++ src/routes/ReviewsRoutes.ts
@@ -0,0 +1,42 @@
+import * as express from "express";
+import { IReviewController } from "../controllers/ReviewController";
+
+/**
+ * Review express Routes.
+ * Defines our API routes.
+ * @author Mario Juez <mario@mjuez.com>
+ */
+export class ReviewRoutes {
+
+    /** Review controller. */
+    private readonly _controller: IReviewController;
+
+    /** Express Router. */
+    private readonly _router: express.Router;
+
+    /**
+     * Class constructor. Injects controller and router dependencies.
+     * @param controller    Review controller.
+     * @param router        A express router.
+     */
+    constructor(controller: IReviewController, router: express.Router) {
+        this._controller = controller;
+        this._router = router;
+    }
+
+    /**
+     * Gets Review API routes.
+     * @returns  the router.
+     */
+    public get routes(): express.Router {
+        const router: express.Router = this._router;
+        const controller: IReviewController = this._controller;
+
+        router.get("/reviews/filter/user/:userlogin/stats/alltime", (req: express.Request, res: express.Response) => {
+            controller.getAllTimeStatsByUser(req, res);
+        });
+
+        return router;
+    }
+
+}
