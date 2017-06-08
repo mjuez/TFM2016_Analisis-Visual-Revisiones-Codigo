8f43e5eb370ac1a55a324ffd88c2b7807ee1f806
Mario Juez <mjuezath@gmail.com>
2017-06-08 12:38:40 +0200

    using new interface #65.

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -25,8 +25,7 @@ export class ReviewController extends AbstractController implements IReviewContr
         const service: IStatsService = this._services.stats;
 
         try {
-            const filter: any = { "user.login": userLogin };
-            const stats: any = await service.getReviewsAllTimeStats(filter);
+            const stats: any = await service.getReviewsStatsByUser(userLogin);
             res.json(stats);
         } catch (error) {
             console.log(error);
