49a776546c8fbdb3926598c45a8c2bca51d296ce
Mario Juez <mjuezath@gmail.com>
2017-06-13 11:51:10 +0200

    refactored and commented #70.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,26 +11,103 @@ import { RoutesUtil } from "../app/util/RoutesUtil";
  */
 export interface IUserController {
 
+    /**
+     * Gets an user entity and returns it as JSON data
+     * through HTTP response.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     get(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by date depending the
+     * direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getPage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by name depending the
+     * direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getByNamePage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by number of pull requests
+     * depending the direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getByPullRequestsPage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by number of reviews
+     * depending the direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getByReviewsPage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by number of reviews of
+     * a specific state (APPROVED, COMMENTED, CHANGES_REQUESTED, DISMISSED)
+     * depending the direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getByReviewsByStatePage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets a page of user entities and returns it as 
+     * JSON data through HTTP response.
+     * User entities are ordered by number of review comments
+     * depending the direction parameter from HTTP request.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getByReviewCommentsPage(req: Request, res: Response): Promise<void>;
+
+    /**
+     * Gets an object with user mean statistics and 
+     * returns it as JSON data through HTTP response.
+     * @async
+     * @param req   express request object.
+     * @param res   express response object.
+     */
     getStatsMeans(req: Request, res: Response): Promise<void>;
 
 }
 
 /**
  * User controller.
- * Defines User requests handling.
+ * Defines User HTTP requests handling.
  * @extends AbstractController.
  * @implements IUserController.
  */
 export class UserController extends AbstractController implements IUserController {
 
-    public async get(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public get = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
         const username: string = req.params.username;
 
@@ -43,62 +120,48 @@ export class UserController extends AbstractController implements IUserControlle
         }
     }
 
-    public async getPage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getPage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersPage(page, direction);
-        }
+        await this.getOrderedPage(req, res, service, service.getUsersPage);
     }
 
-    public async getByNamePage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getByNamePage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersByNamePage(page, direction);
-        }
+        await this.getOrderedPage(req, res, service, service.getUsersByNamePage);
     }
 
-    public async getByPullRequestsPage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getByPullRequestsPage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersByPullRequestsPage(page, direction);
-        }
+        await this.getOrderedPage(req, res, service, service.getUsersByPullRequestsPage);
     }
 
-    public async getByReviewsPage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getByReviewsPage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersByReviewsPage(page, direction);
-        }
+        await this.getOrderedPage(req, res, service, service.getUsersByReviewsPage);
     }
 
-    public async getByReviewsByStatePage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getByReviewsByStatePage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
         const state: string = req.params.state;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersByReviewsByStatePage(page, state, direction);
+        const handler = async (page: number, direction: number): Promise<IUserEntity[]> => {
+            return await service.getUsersByReviewsByStatePage(page, state, direction);
         }
+        await this.getOrderedPage(req, res, service, handler);
     }
 
-    public async getByReviewCommentsPage(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getByReviewCommentsPage = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
-        await this.getOrderedPage(req, res, service, handler);
-
-        async function handler(page: number, direction: number): Promise<IUserEntity[]>{
-            return service.getUsersByReviewCommentsPage(page, direction);
-        }
+        await this.getOrderedPage(req, res, service, service.getUsersByReviewCommentsPage);
     }
 
-    public async getStatsMeans(req: Request, res: Response): Promise<void> {
+    /** @inheritdoc */
+    public getStatsMeans = async (req: Request, res: Response): Promise<void> => {
         const service: IUserService = this._services.user;
         try {
             const means: Object = await service.getUsersStatsMeans();
