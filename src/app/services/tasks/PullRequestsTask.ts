12bc440bc8799d11b3803b78288b6a6bf0aca932
Mario Juez <mjuezath@gmail.com>
2017-06-16 14:51:40 +0200

    commented and refactored #70 #72.

--- src/app/services/tasks/PullRequestsTask.ts
+++ src/app/services/tasks/PullRequestsTask.ts
@@ -1,57 +1,36 @@
 import { ITask } from "./ITask";
-import { GitHubTask } from "./GitHubTask";
 import { IPullRequestEntity, PullRequestEntity } from "../../entities/PullRequestEntity";
-import { ITaskEntity } from "../../entities/TaskEntity";
-import { ITaskRepository } from "../../data/TaskRepository";
-import { IPullRequestRepository } from "../../data/PullRequestRepository";
-import { IPullRequestService } from "../../services/PullRequestService";
-import {
-    RepositoryPullRequestFilter,
-    PullRequestFilterFactory
-} from "../../data/filters/PullRequestFilter";
-import { GitHubUtil } from "../../util/GitHubUtil";
+import { AbstractPullRequestTask } from "./AbstractPullRequestTask";
 import * as GitHubAPI from "github";
 
-interface Repositories {
-    pull: IPullRequestRepository,
-    task: ITaskRepository
-}
-
+/**
+ * Pull Requests Task interface.
+ * 
+ * This task type is intended to obtain all data of every
+ * pull request of a repository. Is necessary because when
+ * getting all pull requests paginated from GitHub API, some
+ * pull request data is not provided.
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
+ */
 export interface IPullRequestsTask extends ITask { }
 
-export class PullRequestsTask extends GitHubTask implements IPullRequestsTask {
+/**
+ * Pull Requests task implementation.
+ * 
+ * @author Mario Juez <mario@mjuez.com>
+ */
+export class PullRequestsTask extends AbstractPullRequestTask implements IPullRequestsTask {
 
-    private readonly _repositories: Repositories;
-
-    private readonly _pullRequestService: IPullRequestService;
-
-    constructor(repos: Repositories, pullRequestService: IPullRequestService, api?: GitHubAPI, apiAuth?: GitHubAPI.Auth) {
-        super(repos.task, api, apiAuth);
-        this._repositories = repos;
-        this._pullRequestService = pullRequestService;
-    }
-
-    public async run(): Promise<void> {
-        const pullRepo: IPullRequestRepository = this._repositories.pull;
-        const filter: RepositoryPullRequestFilter =
-            PullRequestFilterFactory.createRepository({ owner: this.entity.owner, repository: this.entity.repository });
-        const startingFrom: number = this.entity.lastProcessed;
-        try {
-            console.log('Starting pull requests task...');
-            await this.startTask();
-            const numPages: number = await pullRepo.numPages(filter, startingFrom);
-            for (let page: number = 1; page <= numPages; page++) {
-                const pulls: IPullRequestEntity[] = await pullRepo.retrieve({ filter, page, startingFrom });
-                const success: boolean = await this.processPullRequests(pulls);
-                if (!success) return;
-            }
-            await this.completeTask();
-        } catch (error) {
-            this.emit("db:error", error);
-        }
-    }
-
-    private async processPullRequests(pulls: IPullRequestEntity[]): Promise<boolean> {
+    /**
+     * Processes a pull request entity list.
+     * Each pull request fires a GitHub API call for obtaining
+     * all data of that pull request.
+     * @async
+     * @param pulls List of pull request entities.
+     * @returns if successfull processing.
+     */
+    protected async processPullRequests(pulls: IPullRequestEntity[]): Promise<boolean> {
         for (let i: number = 0; i < pulls.length; i++) {
             let pull: IPullRequestEntity = pulls[i];
             try {
@@ -68,6 +47,13 @@ export class PullRequestsTask extends GitHubTask implements IPullRequestsTask {
         return true;
     }
 
+    /**
+     * Makes a GitHub API call for obtaining all pull request
+     * data given its number.
+     * @async
+     * @param pullNumber Pull Request number.
+     * @returns obtained pull request entity.
+     */
     private async makeApiCall(pullNumber: number): Promise<IPullRequestEntity> {
         try {
             const pullData: any = await this.API.pullRequests.get(<GitHubAPI.PullRequestsGetParams>{
