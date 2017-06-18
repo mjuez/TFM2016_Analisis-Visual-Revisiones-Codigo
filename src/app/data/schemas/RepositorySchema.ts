452cde2ebf15e549560b72ec38a1294e47711124
Mario Juez <mjuezath@gmail.com>
2017-06-18 13:35:50 +0200

    extending abstractschema.

--- src/app/data/schemas/RepositorySchema.ts
+++ src/app/data/schemas/RepositorySchema.ts
@@ -1,57 +1,55 @@
 import * as mongoose from "mongoose";
+import { AbstractSchema } from "./AbstractSchema";
 
 /**
  * Repository Schema class. Defines the schema for
  * a Repository.
- * @author Mario Juez <mario@mjuez.com>
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
  */
-export class RepositorySchema {
+export class RepositorySchema extends AbstractSchema {
 
-    /** Gets the Repository Schema. */
-    public static get schema(): mongoose.Schema {
-        let schema = new mongoose.Schema({
-            id: {
-                type: Number,
-                index: true
-            },
-            owner: {
-                login: String,
-                id: Number
-            },
-            name: String,
-            full_name: String,
-            description: String,
-            private: Boolean,
-            fork: Boolean,
-            html_url: String,
-            language: String,
-            forks_count: Number,
-            stargazers_count: Number,
-            watchers_count: Number,
-            suscribers_count: Number,
-            network_count: Number,
-            size: Number,
-            default_branch: String,
-            open_issues_count: Number,
-            pull_requests_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_count: {
-                type: Number,
-                default: 0
-            },
-            review_comments_count: {
-                type: Number,
-                default: 0
-            },
-            topics: [String],
-            pushed_at: Date,
-            created_at: Date,
-            updated_at: Date
-        });
-
-        return schema;
-    }
+    /** Repository Schema definition. */
+    protected static _schemaDefinition: mongoose.SchemaDefinition = {
+        id: {
+            type: Number,
+            index: true
+        },
+        owner: {
+            login: String,
+            id: Number
+        },
+        name: String,
+        full_name: String,
+        description: String,
+        private: Boolean,
+        fork: Boolean,
+        html_url: String,
+        language: String,
+        forks_count: Number,
+        stargazers_count: Number,
+        watchers_count: Number,
+        suscribers_count: Number,
+        network_count: Number,
+        size: Number,
+        default_branch: String,
+        open_issues_count: Number,
+        pull_requests_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_count: {
+            type: Number,
+            default: 0
+        },
+        review_comments_count: {
+            type: Number,
+            default: 0
+        },
+        topics: [String],
+        pushed_at: Date,
+        created_at: Date,
+        updated_at: Date
+    };
 
 }
