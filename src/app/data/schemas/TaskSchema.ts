74397586b4e623b3ad38eabdab56d0ea93a39eb8
Mario Juez <mjuezath@gmail.com>
2017-06-18 13:40:38 +0200

    extending abstractschema.

--- src/app/data/schemas/TaskSchema.ts
+++ src/app/data/schemas/TaskSchema.ts
@@ -1,39 +1,36 @@
 import * as mongoose from "mongoose";
-import { TaskType } from "../../entities/enum/TaskType";
 import { TaskRepository } from "../TaskRepository";
+import { AbstractSchema } from "./AbstractSchema";
 
 /**
  * Task Schema class. Defines the schema for
  * a Task.
- * @author Mario Juez <mario@mjuez.com>
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
  */
-export class TaskSchema {
+export class TaskSchema extends AbstractSchema {
 
-    /** Gets the Task Schema. */
-    public static get schema(): mongoose.Schema {
-        let schema = new mongoose.Schema({
-            type: {
-                type: Number,
-                enum: ['ALL', 'PULL_REQUESTS', 'REVIEWS', 'REVIEW_COMMENTS', 'USERS', 'USERS_PULLS', 'USERS_REVIEWS', 'USERS_REVIEW_COMMENTS', 'REPOSITORY']
-            },
-            is_completed: Boolean,
-            creation_date: Date,
-            start_date: Date,
-            end_date: Date,
-            owner: String,
-            repository: String,
-            current_page: {
-                type: Number,
-                default: 1
-            },
-            parent: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: TaskRepository.COLLECTION_NAME
-            },
-            last_processed: Number
-        });
-
-        return schema;
-    }
+    /** Task Schema definition. */
+    protected static _schemaDefinition: mongoose.SchemaDefinition = {
+        type: {
+            type: Number,
+            enum: ['ALL', 'PULL_REQUESTS', 'REVIEWS', 'REVIEW_COMMENTS', 'USERS', 'USERS_PULLS', 'USERS_REVIEWS', 'USERS_REVIEW_COMMENTS', 'REPOSITORY']
+        },
+        is_completed: Boolean,
+        creation_date: Date,
+        start_date: Date,
+        end_date: Date,
+        owner: String,
+        repository: String,
+        current_page: {
+            type: Number,
+            default: 1
+        },
+        parent: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: TaskRepository.COLLECTION_NAME
+        },
+        last_processed: Number
+    };
 
 }
--- src/app/data/schemas/UserSchema.ts
+++ src/app/data/schemas/UserSchema.ts
@@ -1,61 +1,59 @@
 import * as mongoose from "mongoose";
+import { AbstractSchema } from "./AbstractSchema";
 
 /**
  * User Schema class. Defines the schema for
  * a User.
- * @author Mario Juez <mario@mjuez.com>
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
  */
-export class UserSchema {
+export class UserSchema extends AbstractSchema {
 
-    /** Gets the User Schema. */
-    public static get schema(): mongoose.Schema {
-        let schema = new mongoose.Schema({
-            login: String,
-            id: {
-                type: Number,
-                index: true
-            },
-            html_url: String,
-            type: String,
-            name: String,
-            public_repos: Number,
-            public_gists: Number,
-            followers: Number,
-            following: Number,
-            created_at: Date,
-            updated_at: Date,
-            updated_on_task: mongoose.Schema.Types.ObjectId,
-            pull_request_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_approved_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_commented_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_changes_requested_count: {
-                type: Number,
-                default: 0
-            },
-            reviews_dismissed_count: {
-                type: Number,
-                default: 0
-            },
-            review_comments_count: {
-                type: Number,
-                default: 0
-            }
-        });
-
-        return schema;
-    }
+    /** User Schema definition. */
+    protected static _schemaDefinition: mongoose.SchemaDefinition = {
+        login: String,
+        id: {
+            type: Number,
+            index: true
+        },
+        html_url: String,
+        type: String,
+        name: String,
+        public_repos: Number,
+        public_gists: Number,
+        followers: Number,
+        following: Number,
+        created_at: Date,
+        updated_at: Date,
+        updated_on_task: mongoose.Schema.Types.ObjectId,
+        pull_request_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_approved_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_commented_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_changes_requested_count: {
+            type: Number,
+            default: 0
+        },
+        reviews_dismissed_count: {
+            type: Number,
+            default: 0
+        },
+        review_comments_count: {
+            type: Number,
+            default: 0
+        }
+    };
 
 }
