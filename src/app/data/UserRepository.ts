ba62e1584245378e6646cb5d94e1edb47304be70
Mario Juez <mjuezath@gmail.com>
2017-06-18 19:08:42 +0200

    commented user repository #72.

--- src/app/data/UserRepository.ts
+++ src/app/data/UserRepository.ts
@@ -3,27 +3,29 @@ import { AbstractRepository } from "./AbstractRepository";
 import { IUserEntity, UserEntity } from "../entities/UserEntity";
 import { UserDocument } from "../entities/documents/UserDocument";
 import { UserSchema } from "./schemas/UserSchema";
-import { SinglePullRequestFilter } from "./filters/PullRequestFilter";
 import * as mongoose from "mongoose";
 
 /**
  * IUserRepository interface.
  * Defines custom CRUD operations for an User.
- * @author Mario Juez <mario@mjuez.com>
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
  */
 export interface IUserRepository extends IRepository<IUserEntity, UserDocument> {
 
     /**
      * Retrieves an user given its GitHub id.
+     * 
      * @param id    User GitHub id.
-     * @returns a promise that returns an user entity if resolved.
+     * @returns an user entity.
      */
     findById(id: number): Promise<IUserEntity>;
 
     /**
      * Retrieves an user given its login.
+     * 
      * @param login User login.
-     * @returns a promise that returns an user entity if resolved.
+     * @returns an user entity.
      */
     findByLogin(login: string): Promise<IUserEntity>;
 
@@ -31,7 +33,8 @@ export interface IUserRepository extends IRepository<IUserEntity, UserDocument> 
 
 /**
  * User Repository class.
- * @author Mario Juez <mario@mjuez.com>
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
  */
 export class UserRepository extends AbstractRepository<IUserEntity, UserDocument> implements IUserRepository {
 
@@ -41,6 +44,7 @@ export class UserRepository extends AbstractRepository<IUserEntity, UserDocument
     /**
      * Class constructor.
      * Creates the repository using the collection name and the User schema.
+     * 
      * @param model     Optional mongoose model dependency injection.
      */
     constructor(model?: mongoose.Model<UserDocument>) {
@@ -49,8 +53,9 @@ export class UserRepository extends AbstractRepository<IUserEntity, UserDocument
 
     /**
      * Retrieves an user given its GitHub id.
+     * 
      * @param id    User GitHub id.
-     * @returns a promise that returns an user entity if resolved.
+     * @returns an user entity.
      */
     public findById(id: number): Promise<IUserEntity> {
         return this.findOne({ id: id });
@@ -58,25 +63,57 @@ export class UserRepository extends AbstractRepository<IUserEntity, UserDocument
 
     /**
      * Retrieves an user given its login.
+     * 
      * @param login User login.
-     * @returns a promise that returns an user entity if resolved.
+     * @returns an user entity.
      */
     public findByLogin(login: string): Promise<IUserEntity> {
         return this.findOne({ login: login });
     }
 
+    /**
+     * Obtains the number of pages given a filter and 
+     * a starting from value.
+     * It allows to count the number of pages starting
+     * from a specific user id.
+     * 
+     * @param filter        filtering options.
+     * @param startingFrom  starting from value.
+     * @returns number of pages.
+     */
     public async numPages(filter: Object = {}, startingFrom: number = 0): Promise<number> {
         return this._numPages(filter, startingFrom, 'id');
     }
 
+    /**
+     * Converts an user document to a
+     * user entity.
+     * 
+     * @param document  user document.
+     * @returns an user entity.
+     */
     protected convertToEntity(document: UserDocument): IUserEntity {
         return UserEntity.toEntity(document);
     }
 
+    /**
+     * Converts an user document array to an
+     * array of user entities.
+     * 
+     * @param documentArray user document array.
+     * @returns an user entity array.
+     */
     protected convertToEntityArray(documentArray: UserDocument[]): IUserEntity[] {
         return UserEntity.toEntityArray(documentArray);
     }
 
+    /**
+     * Creates a filter for updating an user entity.
+     * The filter sets the id field as the user id.
+     * 
+     * @param item  user entity.
+     * @return update filter.
+     */
     protected updateFilter(item: IUserEntity): Object {
         return { id: item.id };
     }
