b82ace11362f90d6f547b5f6bd9f5833da6366a9
Mario Juez <mjuezath@gmail.com>
2017-06-18 12:45:40 +0200

    commented filters #73.

--- src/app/data/filters/PullRequestFilter.ts
+++ src/app/data/filters/PullRequestFilter.ts
@@ -1,47 +1,78 @@
+/**
+ * Filter for obtaining one pull request given
+ * repository owner, repository name and 
+ * pull request number.
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
+ */
 export interface SinglePullRequestFilter {
+    
+    /** Repository owner login. */
     "base.repo.owner.login": string,
+
+    /** Repository name. */
     "base.repo.name": string,
+
+    /** Pull Request number. */
     number: number
+
 }
 
+/**
+ * Filter for obtaining all pull requests
+ * of a specific repository given its owner
+ * and its name.
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
+ */
 export interface RepositoryPullRequestFilter {
+    
+    /** Repository owner login. */
     "base.repo.owner.login": string,
+
+    /** Repository name. */
     "base.repo.name": string
 }
 
-export interface BetweenDatesPullRequestFilter {
-    "base.repo.owner.login": string,
-    "base.repo.name": string,
-    $and: [
-        { created_at: { $gte: Date } },
-        { created_at: { $lte: Date } }
-    ]
-}
-
+/**
+ * Filter factory.
+ * Builds different types of filters for pull request
+ * retrieving.
+ * 
+ * @author Mario Juez <mario[at]mjuez.com>
+ */
 export class PullRequestFilterFactory {
+
+    /**
+     * Creates a filter for obtaining a single pull request.
+     * 
+     * @param filter    The filter values:
+     *                      - owner -> repository owner login.
+     *                      - repository -> repository name.
+     *                      - number -> pull request number.
+     * @returns a single pull request filter.
+     */
     public static createSingle(filter: { owner: string, repository: string, number: number }): SinglePullRequestFilter {
         return {
             "base.repo.owner.login": filter.owner,
             "base.repo.name": filter.repository,
             number: filter.number
-        }
+        };
     }
 
+    /**
+     * Creates a filter for obtaining all pull requests
+     * from a specific repository.
+     * 
+     * @param filter    The filter values:
+     *                      - owner -> repository owner login.
+     *                      - repository -> repository name.
+     * @returns a repository pull request filter.
+     */
     public static createRepository(filter: { owner: string, repository: string }): RepositoryPullRequestFilter {
         return {
             "base.repo.owner.login": filter.owner,
             "base.repo.name": filter.repository
-        }
-    }
-
-    public static createBetweenDates(filter: { owner: string, repository: string, startDate: Date, endDate: Date }): BetweenDatesPullRequestFilter {
-        return {
-            "base.repo.owner.login": filter.owner,
-            "base.repo.name": filter.repository,
-            $and: [
-                { created_at: { $gte: filter.startDate } },
-                { created_at: { $lte: filter.endDate } }
-            ]
-        }
+        };
     }
 }
