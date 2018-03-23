41632c01af6c2cef44dd247502b524b9b5ee9916
Mario Juez <mjuezath@gmail.com>
2018-03-23 13:05:46 +0100

    fixed tslint issues.

--- src/app/services/tasks/ITask.ts
+++ src/app/services/tasks/ITask.ts
@@ -3,17 +3,17 @@ import * as Events from "events";
 
 /**
  * Task interface.
- * 
+ *
  * @author Mario Juez <mario[at]mjuez.com>
  */
 export interface ITask extends Events.EventEmitter {
-    
+
     /** Task entity. */
     entity: ITaskEntity;
-    
+
     /**
      * Task entity setter.
-     * 
+     *
      * @param entity task entity.
      */
     setEntity(entity: ITaskEntity): Promise<void>;
@@ -26,7 +26,7 @@ export interface ITask extends Events.EventEmitter {
 
     /**
      * Updates the current page of the task.
-     * 
+     *
      * @param pageNumber    new current page.
      */
     updateCurrentPage(pageNumber: number): Promise<void>;
